841a044f468f1076ce84326c895b3c27bf29a54f
XrustoforK <oleksandr.Kyryliuk@bureau.kyiv.ua>
2022-08-03 18:19:43 +0300

    insert mongoos modules

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,9 +1,17 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly configService: ConfigService){}
+
+	// @Get('get/:alias')
+	// async get(@Param('alias') alias: string): Promise<TopPageModel> {
+	// 	this.configService.get('TEST');
+	// }
+
 	@Post('create')
 	async create(@Body() dto: Omit<TopPageModel, "_id">){
 
